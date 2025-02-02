aa1c94df361f2ec75b7a659eb82d9af1462b2d32
Lendy <lendyjavier04@hotmail.com>
2025-02-02 14:36:01 -0400

    solucion: Configuración de PostgreSQL en Render con Sequelize y Pool

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,17 @@
-import pg from 'pg'
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 
 const pool = new pg.Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
-})
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
 
-export { pool }
+export { pool };
--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -6,7 +6,13 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "postgres",
-  logging: false, 
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
 });
 
 export default sequelize;
